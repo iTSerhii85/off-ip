eee10dd057761542c94219b677f43e07792af428
Aleksey0808 <103374981+Aleksey0808@users.noreply.github.com>
2022-10-11 17:41:49 +0000

    Deploying to gh-pages from @ Aleksey0808/off-ip@7bcae8728cb43a0efa3544b4c972b00b6326826c 🚀

---
+++ index.bc622c52.js
@@ -0,0 +1,2 @@
+!function(){var e={openModalBtn:document.querySelector("[data-modal-buy-now-open]"),openModalBtn1:document.querySelector("[data-modal-buy-now-open1]"),closeModalBtn:document.querySelector("[data-modal-buy-now-close]"),modal:document.querySelector("[data-modal-buy-now]")};function o(){e.modal.classList.toggle("is-hidden")}e.openModalBtn.addEventListener("click",o),e.closeModalBtn.addEventListener("click",o),e.openModalBtn1.addEventListener("click",o)}();
+//# sourceMappingURL=index.bc622c52.js.map
